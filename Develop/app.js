205dbf55d37df9195f992edc08f71e26a8de3fb3
NZerr <nathan.zerr@gmail.com>
2021-06-13 18:37:06 -0400

    added additional comments

--- Develop/app.js
+++ Develop/app.js
@@ -17,7 +17,7 @@ console.log(outputPath);
 const render = require("./lib/htmlRenderer");
 const { type } = require("os");
 
-if (!fs.existsSync(OUTPUT_DIR)){
+if (!fs.existsSync(OUTPUT_DIR)) {
   fs.mkdirSync(OUTPUT_DIR);
 };
 
@@ -25,7 +25,7 @@ if (!fs.existsSync(OUTPUT_DIR)){
 const team = [];
 
 
-
+// Questions for Manager
 const managerQuestions = [
   {
     type: 'input',
@@ -49,6 +49,7 @@ const managerQuestions = [
   }
 ];
 
+// Questions for Engineers
 const engineerQuestions = [
   {
     type: 'input',
@@ -72,6 +73,7 @@ const engineerQuestions = [
   },
 ];
 
+// Questions for Interns
 const internQuestions = [
   {
     type: 'input',
@@ -95,54 +97,54 @@ const internQuestions = [
   },
 ];
 
-
+// Function that prompts questions based on employee role. Writes file after done adding employees
 const createTeam = (answer) => {
   console.log(answer);
   if (answer.role === 'Manager') {
     inquirer.prompt(managerQuestions)
-    .then(function (choice) {
-      console.log(choice);
+      .then(function (choice) {
+        console.log(choice);
 
-      const newManager = new Manager(
-        choice.name,
-        choice.id,
-        choice.email,
-        choice.officeNumber
-      );
-      team.push(newManager);
-      console.log('This manager has been added');
-      init();
-    })
+        const newManager = new Manager(
+          choice.name,
+          choice.id,
+          choice.email,
+          choice.officeNumber
+        );
+        team.push(newManager);
+        console.log('This manager has been added');
+        init();
+      })
   } else if (answer.role === 'Engineer') {
     inquirer.prompt(engineerQuestions)
-    .then(function (choice) {
-      console.log(choice);
+      .then(function (choice) {
+        console.log(choice);
 
-      const newEngineer = new Engineer(
-        choice.name,
-        choice.id,
-        choice.email,
-        choice.gitHub
-      );
-      team.push(newEngineer);
-      console.log('This engineer has been added');
-      init();
-    })
+        const newEngineer = new Engineer(
+          choice.name,
+          choice.id,
+          choice.email,
+          choice.gitHub
+        );
+        team.push(newEngineer);
+        console.log('This engineer has been added');
+        init();
+      })
   } else if (answer.role === 'Intern') {
     inquirer.prompt(internQuestions)
-    .then(function (choice) {
-      console.log(choice);
+      .then(function (choice) {
+        console.log(choice);
 
-      const newIntern = new Intern(
-        choice.name,
-        choice.id,
-        choice.email,
-        choice.school
-      );
-      team.push(newIntern);
-      console.log('This intern has been added');
-      init();
-    })
+        const newIntern = new Intern(
+          choice.name,
+          choice.id,
+          choice.email,
+          choice.school
+        );
+        team.push(newIntern);
+        console.log('This intern has been added');
+        init();
+      })
   } else {
     fs.writeFileSync(outputPath, render(team), (err) => {
       if (err) throw err;
@@ -151,46 +153,26 @@ const createTeam = (answer) => {
     );
   }
 };
-
+// Allows user to select employee role
 const chooseRole = [
-      {
-      type: "rawlist",
-      message: "What type of employee would you like to create?",
-      name: "role",
-      choices: 
+  {
+    type: "rawlist",
+    message: "What type of employee would you like to create?",
+    name: "role",
+    choices:
       [
         "Manager",
         "Engineer",
         "Intern",
         "Done creating employees"
       ],
-    },];
+  },];
 
+// Starts program and begins to prompt questions
 const init = () => {
   inquirer.prompt(chooseRole)
-  .then(createTeam);
+    .then(createTeam);
 };
 
 init();
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
